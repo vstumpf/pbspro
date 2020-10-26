cad140c0ce18299aa76aabadaa7d674eee642c1a
Vincent Stumpf <vincents.995@gmail.com>
2020-10-26 16:06:42 -0700

    Rename resreq to res

--- poc/main.cpp
+++ poc/main.cpp
@@ -16,30 +16,30 @@ int main() {
     }
 
     // a job's requested resources
-    std::vector<std::shared_ptr<Resource>> resreqs;
-    resreqs.push_back(std::make_shared<LongResource>("ncpus", "30"));
-    resreqs.push_back(std::make_shared<StringResource>("vnode", "shecil"));
-    resreqs.push_back(std::make_shared<StringArrayResource>("colors", "red,green,blue"));
+    std::vector<std::shared_ptr<Resource>> res;
+    res.push_back(std::make_shared<LongResource>("ncpus", "30"));
+    res.push_back(std::make_shared<StringResource>("vnode", "shecil"));
+    res.push_back(std::make_shared<StringArrayResource>("colors", "red,green,blue"));
 
-    for (const auto &resreq : resreqs) {
+    for (const auto &resreq : res) {
         switch(resreq->getType()) {
             case ResourceType::rescTypeLong:
             {
-                auto long_resreq = static_cast<LongResource *>(resreq.get());
-                printf("Name [%s] | Type [%d] | Value [%d]\n", long_resreq->getName().c_str(), long_resreq->getType(), long_resreq->getLongValue());
+                auto long_res = static_cast<LongResource *>(resreq.get());
+                printf("Name [%s] | Type [%d] | Value [%d]\n", long_res->getName().c_str(), long_res->getType(), long_res->getLongValue());
                 break;
             }
             case ResourceType::rescTypeString:
             {
-                auto string_resreq = static_cast<StringResource *>(resreq.get());
-                printf("Name [%s] | Type [%d] | Value [%s]\n", string_resreq->getName().c_str(), string_resreq->getType(), string_resreq->getStringValue().c_str());
+                auto string_res = static_cast<StringResource *>(resreq.get());
+                printf("Name [%s] | Type [%d] | Value [%s]\n", string_res->getName().c_str(), string_res->getType(), string_res->getStringValue().c_str());
                 break;
             }
             case ResourceType::rescTypeStringArray:
             {
-                auto strarr_resreq = static_cast<StringArrayResource *>(resreq.get());
-                printf("Name [%s] | Type [%d] | Values ", strarr_resreq->getName().c_str(), strarr_resreq->getType());
-                auto strarr = strarr_resreq->getStringArrayValue();
+                auto strarr_res = static_cast<StringArrayResource *>(resreq.get());
+                printf("Name [%s] | Type [%d] | Values ", strarr_res->getName().c_str(), strarr_res->getType());
+                auto strarr = strarr_res->getStringArrayValue();
                 for (auto it = strarr.begin(); it != strarr.end(); ++it) {
                     printf("[%s] ", it->c_str());
                 }
